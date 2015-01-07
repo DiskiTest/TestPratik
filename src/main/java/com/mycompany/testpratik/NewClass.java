8c5ebfa6c2e32d835d30335a4e4dcef536ebb3a9
caglarturkurka <caglarturkurka@gmail.com>
2015-01-07 22:20:25 +0200

    New Feature was added NewClass

--- src/main/java/com/mycompany/testpratik/NewClass.java
+++ src/main/java/com/mycompany/testpratik/NewClass.java
@@ -14,5 +14,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("asdasd");
         System.out.println("Jira Entegrasyon");
+        System.out.println("Jira Entegrasyon 2");
     }
 }
